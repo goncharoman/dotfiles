d130f728014fd7e5f4d0176fa7a0eb91db1303b3
goncharoman <goncharoman@gmail.com>
2025-05-11 17:44:48 +0300

    added configuration for protobuf development

---
+++ nvim/lua/config/proto.lua
@@ -0,0 +1,24 @@
+return {
+
+  ---@type LazySpec
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+          "proto",
+        })
+      end
+    end,
+  },
+
+  ---@type LazySpec
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "buf",
+      })
+    end,
+  },
+}
