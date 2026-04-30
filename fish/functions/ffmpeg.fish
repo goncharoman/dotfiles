e56225431158b5b23b3ef1cac71a826ef159afa3
goncharoman <goncharoman@gmail.com>
2026-05-01 00:47:29 +0300

    refactor(fish): removed additional functions

--- fish/functions/ffmpeg.fish
+++
@@ -1,3 +0,0 @@
-function ffmpeg
-    podman run --rm -it -v $(pwd):/data -w /data linuxserver/ffmpeg:version-8.0-cli $argv
-end
--- fish/functions/ffprobe.fish
+++
@@ -1,3 +0,0 @@
-function ffprobe
-    podman run --rm -it --entrypoint ffprobe -v $(pwd):/data -w /data linuxserver/ffmpeg:version-8.0-cli $argv
-end
